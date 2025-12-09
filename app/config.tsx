4f9d5d3075f2b8120272cbc4ce6ee6b547c79b49
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-09 15:23:54 +0100

    Update config.tsx

--- app/config.tsx
+++ app/config.tsx
@@ -8,7 +8,7 @@ const projectId = 'ae28066373c7e52fc677be71ca'   //← replace with yours
 
 const metadata = {
   name: 'Base Token dApp',
-  desription: 'Send BK tkens istantly on Base Chain',
+  desription: 'Send BK kens istantly on Base Chain',
   url: 'https://your-app.vercel.app',
   icons: ['avatars.githubusercontent.com']
 }
