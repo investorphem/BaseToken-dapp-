4cb935b569084db2f2e343c52db97c5398075539
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-11 20:46:56 +0100

    Update config.tsx

--- app/config.tsx
+++ app/config.tsx
@@ -1,4 +1,4 @@
-'use client'; // This is the requi
+'use client'; // This is the requ
 import { createAppKit } from '@reown/appkit/react'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { base } from '@reown/appkit/networks'
