09919b0d93584575eaff67349ff611dc8f8c58c7
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-11 20:40:36 +0100

    Update config.tsx

--- app/config.tsx
+++ app/config.tsx
@@ -1,4 +1,4 @@
-'use client'; // This is the required fix for the "Applicat
+'use client'; // This is the required fix for the "Applica
 import { createAppKit } from '@reown/appkit/react'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { base } from '@reown/appkit/networks'
