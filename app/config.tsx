559f09fb285e564a7ca6f108899b00076d802637
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-24 07:35:27 +0100

    Update config.tsx

--- app/config.tsx
+++ app/config.tsx
@@ -1,4 +1,4 @@
-'use client'; // This is the required fix for the "Application error"
+'use client'; // This is the required fix for the "Application errr"
 
 import { createAppKit } from '@reown/appkit/react'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
